9ec00b7888ec790245dd6ff7d546e0369b6ef070
Jan Schulte <hello@unexpected-co.de>
2015-07-06 20:15:00 +0200

    Initial Commit

---
+++ streamie/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  streamie
+//
+//  Created by Jan Schulte on 06/07/15.
+//  Copyright (c) 2015 schultyy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
---
+++ streamie/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  streamie
+//
+//  Created by Jan Schulte on 06/07/15.
+//  Copyright (c) 2015 schultyy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
